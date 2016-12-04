f4b4080011d90f05ee265e37f62f6e9c734a4177
Michael Vander Lugt <mvlugt@umich.edu>
2016-12-04 00:41:28 -0500

    article download and parsing setup

--- MANA.py
+++ MANA.py
@@ -1,11 +1,16 @@
 from bs4 import BeautifulSoup
 from urllib import request
 import json
-from newspaper import 
+from newspaper import Article
+
+# Install newspaper for OSX 
+# $ brew install libxml2 libxslt
+# $ brew install libtiff libjpeg webp little-cms2
+# $ pip3 install newspaper3k
+# $ curl https://raw.githubusercontent.com/codelucas/newspaper/master/download_corpora.py | python3
 
 
-
-def get_articles():
+def get_article_urls():
     url = "https://newsapi.org/v1/articles?source=techcrunch&apiKey=66e0dd0ca71043c09db31c1733d53a7e"
     response = str(request.urlopen(url).read().decode('utf-8'))
     response = str(response.encode('utf-8', 'replace'))
@@ -19,9 +24,23 @@ def get_articles():
     article_urls = [article["url"] for article in response_json["articles"]]
     return article_urls
 
+def get_parsed_articles(article_urls):
+    articles = []
+    for url in article_urls:
+        article = Article(url)
+        # Single-threaded for now
+        article.download()
+        article.parse()
+        # Hack to detect parsing error for now
+        if article.title != "":
+            articles.append(article)
+    return articles
 
 def main():
-    articles = get_articles()
-    print(repr(articles))
-
+    article_urls = get_article_urls()
+    parsed_articles = get_parsed_articles(article_urls)
+    # NLP Example:
+    for article in parsed_articles:
+        article.nlp()
+        print(article.keywords)
 main()
