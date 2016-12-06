a316664c1bf1f5ac046e3c1d8fcfdf3957ddf488
danilvnh <danilvnh@umich.edu>
2016-12-06 13:50:53 -0500

    commented things out for testing

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 
 import requests
 from flask import Flask, request
-from manaportion import *
+# from manaportion import *
 
 app = Flask(__name__)
 
@@ -40,44 +40,44 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-                    current_user = db.get_user(sender_id)
-                    # If current_user returns null, then this user does not exist in our database yet
-                    if not current_user:
-                        # 1). Create a user
-                        current_user = db.create_user(sender_id)
-                        # 2). Send a lil greeting 
-                        send_message(sender_id, "Hi I'm MANA! Please tell me which of these articles interested you most")
-                        article_list_message = ""
-                        # 3). Send the list of our default initial articles
-                        for i in range(0, len(ARTICLES)):
-                            article_list_message += (str(i) + ": " + ARTICLES[i] + "\n")
-                        send_message(sender_id, article_list_message)
-                    else:
-                        # Get the numbers of the article the user likes
-                        articles_user_likes = [int(s) for s in message_text.split() if s.isdigit()]
-                        send_message(sender_id, "Thanks for giving me that feedback.")
-                        for index in articles_user_likes:
-                            # For each article the user likes, grab the keywords and update the user profile
-                            url = ARTICLES[index]
-                            key_words = content.get_stats(url)
-                            db.update_stats(sender_id, key_words)
+                    # current_user = db.get_user(sender_id)
+                    # # If current_user returns null, then this user does not exist in our database yet
+                    # if not current_user:
+                    #     # 1). Create a user
+                    #     current_user = db.create_user(sender_id)
+                    #     # 2). Send a lil greeting 
+                    #     send_message(sender_id, "Hi I'm MANA! Please tell me which of these articles interested you most")
+                    #     article_list_message = ""
+                    #     # 3). Send the list of our default initial articles
+                    #     for i in range(0, len(ARTICLES)):
+                    #         article_list_message += (str(i) + ": " + ARTICLES[i] + "\n")
+                    #     send_message(sender_id, article_list_message)
+                    # else:
+                    #     # Get the numbers of the article the user likes
+                    #     articles_user_likes = [int(s) for s in message_text.split() if s.isdigit()]
+                    #     send_message(sender_id, "Thanks for giving me that feedback.")
+                    #     for index in articles_user_likes:
+                    #         # For each article the user likes, grab the keywords and update the user profile
+                    #         url = ARTICLES[index]
+                    #         key_words = content.get_stats(url)
+                    #         db.update_stats(sender_id, key_words)
                         
-                        # This should reflect the updated user profile
-                        ARTICLES = get_relevant_urls(user)
+                    #     # This should reflect the updated user profile
+                    #     ARTICLES = get_relevant_urls(user)
 
-                        # Filter for relevance here?
-                        for article in ARTICLES:
-                            # I made this function up lol idk if it will exist
-                            if quality.get_quality(article) == 0:
-                                ARTICLES.remove(article)
+                    #     # Filter for relevance here?
+                    #     for article in ARTICLES:
+                    #         # I made this function up lol idk if it will exist
+                    #         if quality.get_quality(article) == 0:
+                    #             ARTICLES.remove(article)
 
 
-                        send_message(sender_id, "I found these articles I thought you might like. As always, please let me know which articles interested you most")
-                        article_list_message = ""
-                        # 3). Send the list of our default initial articles
-                        for i in range(0, len(new_recs)):
-                            article_list_message += (str(i) + ": " + ARTICLES[i] + "\n")
-                        send_message(sender_id, article_list_message)
+                    #     send_message(sender_id, "I found these articles I thought you might like. As always, please let me know which articles interested you most")
+                    #     article_list_message = ""
+                    #     # 3). Send the list of our default initial articles
+                    #     for i in range(0, len(new_recs)):
+                    #         article_list_message += (str(i) + ": " + ARTICLES[i] + "\n")
+                    #     send_message(sender_id, article_list_message)
                             
                             
                 if messaging_event.get("delivery"):  # delivery confirmation
