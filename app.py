d2234c8391df02acd58480a9be8da75d1bd52152
danilvnh <danilvnh@umich.edu>
2016-12-08 07:16:59 -0500

    nothing like an early morining commit

--- app.py
+++ app.py
@@ -76,12 +76,13 @@ def webhook():
                     log(str(current_user.keywords))
                     # 1). gets quality
                     #quality_articles = quality.filterArticles(ARTICLES)
+                    qual_message = "Out of the articles you sent me, \n\thttp://www.cnn.com/2016/12/06/politics/obama-trump-terrorism-views/index.html\n\thttp://www.cnn.com/2016/12/06/entertainment/grammy-nominations-2017/index.html\nseem to be quality articles"
                     
                     # 2). Returns a list of tuples (cosine, url)
-                    results = content.__top_n_articles(quality_articles, current_user, 2)
+                    results = content.__top_n_articles(ARTICLES, current_user, 2)
                     # summary = results[len(results)-1].summary
                     summary = "Jk"
-                    rel_message = "The article most relevant to you is: \n" + results[len(results)-1][1] + "\nIt has a relevance weight of: " + str(results[len(results)-1][0]) + "\n\nHere is a little summary: " + summary
+                    rel_message = "The article most relevant to you is: \n\t" + results[len(results)-1][1] + "\nIt has a relevance weight of: " + str(results[len(results)-1][0]) + "\n\nHere is a little summary: " + summary
                     send_message(sender_id, rel_message)
                     
                 else:
