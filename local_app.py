c66342143adc16c123900745b64975642889a9f6
danilvnh <danilvnh@umich.edu>
2016-12-11 18:43:36 -0500

    local runner

--- local_app.py
+++ local_app.py
@@ -10,17 +10,17 @@ from manapotion import user, db, content
 from QA import quality
 
 # list of Article URLS, as a default this should start with our "seed" urls
-ARTICLES = ["https://techcrunch.com/2016/12/08/uber-spells-out-what-causes-riders-to-lose-access-in-new-policy/", "http://www.cnn.com/2016/12/06/entertainment/grammy-nominations-2017/index.html", "http://www.dailytimesgazette.com/36308-2/36308/"]
 
 
-def send_articles():
+
+def send_articles(ARTICLES):
     article_message =  ""
     for i in range(0, len(ARTICLES)):
         article_message += (str(i) + ": " + ARTICLES[i] + "\n")
     print(article_message)
 
 
-def get_feedback(user):
+def get_feedback(user, ARTICLES):
     feedback = input("Which of these articles interest you most? ")
     articles_user_likes = [int(s) for s in feedback.split() if s.isdigit()]
     for index in articles_user_likes:
@@ -43,8 +43,9 @@ def main():
         current_user = db.create_user(name)
         print("Welcome to MANA " + name)
         print("Here are some initial articles, please take a look: ")
-        send_articles()
-        get_feedback(current_user)
+        ARTICLES = ["https://techcrunch.com/2016/12/08/uber-spells-out-what-causes-riders-to-lose-access-in-new-policy/", "http://www.cnn.com/2016/12/06/entertainment/grammy-nominations-2017/index.html", "http://www.dailytimesgazette.com/36308-2/36308/"]
+        send_articles(ARTICLES)
+        get_feedback(current_user, ARTICLES)
         response = input("Would you like to continue using MANA? ")
         if response == "yes":
             go_on = True
@@ -57,9 +58,13 @@ def main():
         print ("Here are some articles I think you might like: ")
     
     while go_on:
-        ARTICLES = quality.filterArticles(content.get_relevant_urls(current_user))
-        send_articles()
-        get_feedback()
+        rel_articles = content.get_relevant_urls(current_user)
+        rel_articles = [item[1] for item in rel_articles]
+        qual_articles = quality.filterArticles(rel_articles)
+        print (qual_articles)
+        ARTICLES = [item.url for item in qual_articles]
+        send_articles(ARTICLES)
+        get_feedback(current_user, ARTICLES)
         response = input("Would you like to continue using MANA? ")
         if response == "yes":
             go_on = True
